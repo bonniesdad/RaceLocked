ea499f6466397e35ea9272444aa0032052d24d18
bonniesdad <bonniesnerdydad@gmail.com>
2026-04-09 17:23:51 +0100

    sync

--- RaceLocked.lua
+++ RaceLocked.lua
@@ -4,10 +4,11 @@ RaceLocked = CreateFrame('Frame')
 RaceLocked:RegisterEvent('ADDON_LOADED')
 RaceLocked:SetScript('OnEvent', function(self, event, loadedAddonName)
   if event == 'ADDON_LOADED' and loadedAddonName == addonName then
-    local achievementPoints = RaceLocked_GetHCATotalPoints()
-    print("|cfff44336[Race Locked]|r Achievement points: " .. achievementPoints)
     RaceLockedDB = RaceLockedDB or {}
     RaceLockedDB.minimapButton = RaceLockedDB.minimapButton or { hide = false }
     RaceLockedDB.guildPeers = RaceLockedDB.guildPeers or {}
+    if RaceLockedDB.showOnScreenLeaderboard == nil then
+      RaceLockedDB.showOnScreenLeaderboard = true
+    end
   end
 end)
