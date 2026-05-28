8585ee75fee8823d5482f0568f3175b0f1f03adf
bonniesdad <bonniesnerdydad@gmail.com>
2026-05-28 14:40:32 +0100

    Don't restrict non gnomes - better reading

--- Functions/GuildFound/TradeRestriction/TradeRestriction.lua
+++ Functions/GuildFound/TradeRestriction/TradeRestriction.lua
@@ -5,31 +5,32 @@ frame:RegisterEvent('AUCTION_HOUSE_SHOW')
 frame:RegisterEvent('MAIL_INBOX_UPDATE')
 
 frame:SetScript('OnEvent', function(self, event, ...)
-  if not RaceLocked_IsInGuildFoundGuild() or not RaceLocked_AmIVerified() then return end
-  if event == 'MAIL_INBOX_UPDATE' then
-    for inboxIndex = GetInboxNumItems(), 1, -1 do
-      local _, _, sender, _, _, _, _, _, _, _, _, isGM = GetInboxHeaderInfo(i)
-      if sender and not isGM then
-        if not RaceLocked_IsPlayerInGuildRoster(sender) then
-          RaceLocked_CancelMailWithMessage(
-            inboxIndex,
-            'Mail from ' .. sender .. ' blocked - not on my Guild.'
-          )
+  if RaceLocked_IsInGuildFoundGuild() and RaceLocked_AmIVerified() then
+    if event == 'MAIL_INBOX_UPDATE' then
+      for inboxIndex = GetInboxNumItems(), 1, -1 do
+        local _, _, sender, _, _, _, _, _, _, _, _, isGM = GetInboxHeaderInfo(i)
+        if sender and not isGM then
+          if not RaceLocked_IsPlayerInGuildRoster(sender) then
+            RaceLocked_CancelMailWithMessage(
+              inboxIndex,
+              'Mail from ' .. sender .. ' blocked - not on my Guild.'
+            )
+          end
         end
       end
-    end
-  elseif event == 'TRADE_SHOW' then
-    local targetName = GetUnitName('npc', true)
-    if not targetName then return end
+    elseif event == 'TRADE_SHOW' then
+      local targetName = GetUnitName('npc', true)
+      if not targetName then return end
 
-    RaceLocked_CanPerformTradeWithPlayer(targetName, function(canTrade, message)
-      if not canTrade then
-        RaceLocked_CancelTradeWithMessage(message)
-      end
-    end)
-  elseif event == 'TRADE_CLOSED' then
-    RaceLocked_EndTradeVerification()
-  elseif event == 'AUCTION_HOUSE_SHOW' then
-    RaceLocked_CancelAuctionHouseWithMessage('Auction House blocked')
+      RaceLocked_CanPerformTradeWithPlayer(targetName, function(canTrade, message)
+        if not canTrade then
+          RaceLocked_CancelTradeWithMessage(message)
+        end
+      end)
+    elseif event == 'TRADE_CLOSED' then
+      RaceLocked_EndTradeVerification()
+    elseif event == 'AUCTION_HOUSE_SHOW' then
+      RaceLocked_CancelAuctionHouseWithMessage('Auction House blocked')
+    end
   end
 end)
